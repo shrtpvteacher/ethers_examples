e27a731ae655206a360b917464e294e179515bbb
Kelly Coldren <shrtpvteacher@protonmail.com>
2023-11-04 15:31:03 -0700

    first commit

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,14 +1,17 @@
 const { ethers } = require("ethers");
+// or as ES6 or Type script
+//  import {ethers} from "ethers";
+const rpcURL = 'https://cloudflare-eth.com/';
 
-const INFURA_ID = ''
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
 
-const address = '0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e'
+const provider = new ethers.providers.JsonRpcProvider();
+// or if wanting to connect to metamask do this: it will wrap a Web3Provider around the window.ethereum object
+// const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+const address = "0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e";
 
 const main = async () => {
-    const balance = await provider.getBalance(address)
-    console.log(`\nETH Balance of ${address} --> ${ethers.utils.formatEther(balance)} ETH\n`)
-}
-
-main()
+  const balance = await provider.getBalance(address);
+  console.log(balance);
+};
 
